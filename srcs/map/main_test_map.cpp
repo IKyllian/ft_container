bd59e42cb88283cdf521ab4bf7d37a91808cd596
IKyllian <35967130+IKyllian@users.noreply.github.com>
2022-02-20 20:34:53 +0100

    created tree class and node class

--- srcs/map/main_test_map.cpp
+++ srcs/map/main_test_map.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <map>
 
+struct classcomp {
+  bool operator() (const char& lhs, const char& rhs) const
+  {return lhs>rhs;}
+};
+
 int main()
 {
-	std::map<char, int> mp;
+	std::map<int, char, classcomp> mp;
 	std::cout << " Size = " << mp.size() << std::endl;
-	mp['a']=10;
-	mp['b']=30;
-	mp['c']=50;
-	mp['f']=70;
+	mp[5]= 'd';
+	mp[3]= 'c';
+	mp[1]= 'a';
+	mp[2]= 'b';
 	std::cout << " Size = " << mp.size() << std::endl;
-	for (std::map<char, int>::iterator it = mp.begin(); it != mp.end(); it++)
+	for (std::map<int, char>::iterator it = mp.begin(); it != mp.end(); it++)
 		std::cout << "Elem First = " << (*it).first << " Second = " << (*it).second << std::endl;
 	std::cout << std::endl;
-	std::map<char, int> second(mp.begin(), mp.end());
-	for (std::map<char, int>::iterator it = mp.begin(); it != mp.end(); it++)
+	std::map<int, char> second(mp.begin(), mp.end());
+	for (std::map<int, char>::iterator it = mp.begin(); it != mp.end(); it++)
 		std::cout << "Elem First = " << (*it).first << " Second = " << (*it).second << std::endl;
 
 	return (0);	
--- srcs/map/map.hpp
+++ srcs/map/map.hpp
@@ -82,13 +82,100 @@ namespace ft
 		return (ft::pair<T1,T2>(x,y));
 	};
 
-	template <class Key, class Comapare>
-	class node
-	{
+	template <typename Key, typename Value, class Compare = std::less<Key> >
+	class mapIterator {
 		public :
 
-		node();
-		node& operator=(const node& src) {
+			typedef node<Key, Compare> node;
+			typedef *node node_ptr;
+
+			typedef Key key_type;
+
+			typedef Value mapped_type
+
+			typedef ft::pair<const key_type, mapped_type> value_type; 
+
+			typedef value_type*			pointer;
+			typedef const value_type*	const_pointer;
+			typedef value_type&			reference;
+			typedef const value_type&	const_reference;
+			typedef Compare key_compare;
+			typedef typename	std::ptrdiff_t difference_type;
+
+
+			mapIterator(){ };
+			mapIterator(pointer ptr){ _ptr = ptr; };
+			mapIterator(const mapIterator &src){ *this = src; };
+			~mapIterator() {};
+
+			mapIterator &operator=(const mapIterator& src) {
+				_ptr = src._ptr;
+				return (*this);
+			};
+	
+			// ---------Increment/Decrement---------
+			mapIterator &operator++(void) {
+				
+				_ptr++;
+				return (*this);
+			}
+			mapIterator operator++(int) {
+				mapIterator tmp = *this;
+				_ptr++;
+				return (tmp);
+			}
+			mapIterator &operator--(void) {
+				_ptr--;
+				return (*this);
+			}
+			mapIterator operator--(int) {
+				mapIterator tmp = *this;
+				_ptr--;
+				return (tmp);
+			}
+			
+			// -----------Dereferencing/Address----------
+			pointer operator->(){ return (_ptr); };
+			pointer operator ->() const { return (_ptr); };		
+			reference operator*(){ return (*(_ptr)); };
+			const_reference operator *() const { return (*_ptr); };
+
+		private :
+			node_ptr _ptr;
+	};
+
+	template <class Key>
+	struct node {
+		node	*_left;
+		node	*_right;
+		node	*_parent;
+		int		_color;
+		Key		_key;
+
+		node(const Key &key) : _left(NULL), _right(NULL), _parent(NULL), _color(ROUGE), _key(key) {};
+	};
+
+	template <class Key, class Compare, class Alloc = std::allocator<ft::node<Key> > >
+	class tree
+	{
+		
+
+		public :
+
+		typedef Key value_type;
+		typedef Compare value_compare;
+		typedef Alloc allocator_type;
+		typedef typename allocator_type::reference reference;
+		typedef typename allocator_type::const_reference const_reference;
+		typedef typename allocator_type::pointer pointer;
+		typedef typename allocator_type::const_pointer const_pointer;
+		typedef std::ptrdiff_t difference_type;
+		typedef std::size_t size_type;
+
+		tree();
+
+		tree(const tree& src);
+		tree& operator=(const tree& src) {
 			if (*this == src)
 				return (this);
 			this->_left = src._left;
@@ -98,89 +185,91 @@ namespace ft
 			this->_key = src._key;
 		};
 
-		bool operator==(const node& src) {
+		bool operator==(const tree& src) {
 			if (this->_key == src._key)
 				return (true);
 			return (false);
 		};
 		
-		~node();
+		~tree();
 
-		node* get_parent() { return (this->_parent) };
-		node* get_grandparent() { 
-			node *p = get_parent();
+		pointer get_parent(pointer n) { return (n->_parent) };
+		pointer get_grandparent(pointer n) { 
+			pointer p = n->get_parent();
 			if (p == NULL) // Noeud sans parent n'a pas de grand parent
 				return (NULL);
 			return (p->get_parent());
 		};
-		node* get_brother() {
-			node *p = get_parent();
-			if (p == NULL) // Noeud sans parent n'a pas de frere
-				return (NULL);
-			if (this == p->_left)
-				return (p->_right);
-			return (p->left);
-		};
-		node* get_oncle() {
-			node *p = get_parent();
-			node *g = get_grandparent();
+		pointer get_left(pointer n) { return (n->_left); };
+		pointer get_right(pointer n) { return (n->_right); };
+		// pointer get_brother(pointer n) {
+		// 	pointer p = n->get_parent();
+		// 	if (p == NULL) // Noeud sans parent n'a pas de frere
+		// 		return (NULL);
+		// 	if (n == p->_left)
+		// 		return (p->_right);
+		// 	return (p->left);
+		// };
+		pointer get_oncle(pointer n) {
+			pointer p = n->get_parent();
+			pointer g = n->get_grandparent();
 			if (g == NULL) // Noeud sans grand parent n'a pas d'oncle
 				return (NULL);
 			return (p->get_brother());
 		};
 
-		void left_rotation() {
-			node *y = this->_right;
+		void left_rotation(pointer x) {
+			pointer y = x->_right;
 
 			// -------- 1 --------
-			this->_right = y->_left;
+			x->_right = y->_left;
 			if (y->_left != NULL)
-				y->_left->_parent = this;
+				y->_left->_parent = x;
 			// -------------------
 
 			// -------- 2 --------
-			y->_parent = this->_parent;
-			if (this->_parent == NULL)
-				this = y;
-			else if (this == this->_parent->_left)
-				this->_parent->_left = y;
+			y->_parent = x->_parent;
+			if (x->_parent == NULL)
+				x = y;
+			else if (x == x->_parent->_left)
+				x->_parent->_left = y;
 			else
-				this->_parent->_right = y;
+				x->_parent->_right = y;
 			// -------------------
 
 			// -------- 3 --------
-			y->_left = this;
-			this->parent = y;
+			y->_left = x;
+			x->parent = y;
 			// -------------------
 		}
 
-		void right_rotation() {
-			node *y = this->_left;
+		void right_rotation(pointer x) {
+			pointer y = x->_left;
 
 			// -------- 1 --------
-			this->_left = y->_right;
+			x->_left = y->_right;
 			if (y->_right != NULL)
-				y->_right->_parent = this;
+				y->_right->_parent = x;
 			// -------------------
 
 			// -------- 2 --------
-			y->_parent = this->_parent;
-			if (this->_parent == NULL)
-				this = y;
-			else if (this == this->_parent->_right)
-				this->_parent->_right = y;
+			y->_parent = x->_parent;
+			if (x->_parent == NULL)
+				x = y;
+			else if (x == x->_parent->_right)
+				x->_parent->_right = y;
 			else
-				this->_parent->_left = y;
+				x->_parent->_left = y;
 			// -------------------
 
 			// -------- 3 --------
-			y->_right = this;
-			this->parent = y;
+			y->_right = x;
+			x->parent = y;
 			// -------------------
 		}
 
-		node *search_node(Key key) {
-			node *tmp = this;
+		pointer search_node(Key key) {
+			pointer tmp = this;
 
 			while (tmp != NULL)
 			{
@@ -194,7 +283,9 @@ namespace ft
 			return (NULL);
 		};
 
-		void insert_repare()
+		pointer get_root() { return (this); };
+
+		void insert_repare(pointer n)
 		{
 			/*
 				Une fois qu'on ajoute un noeud il faut check si toutes les regles sont encore respectés
@@ -214,38 +305,38 @@ namespace ft
 				
 				Source + Schéma : https://fr.wikipedia.org/wiki/Arbre_bicolore#:~:text=Un%20arbre%20bicolore%20est%20un%20arbre%20binaire%20de%20recherche%20dans,est%20soit%20rouge%20soit%20noire.&text=Le%20chemin%20de%20la%20racine,n%C5%93uds%20noirs%20la%20hauteur%20noire 
 			*/
-			if (this->parent() == NULL)
-				this->_color = NOIR;
+			if (n->parent() == NULL)
+				n->_color = NOIR;
 			else if (parent(n)->couleur == NOIR)
 				return ;
-			else if (this->get_oncle()->_color == ROUGE) {
-				this->get_parent()->_color = NOIR;
-				this->get_oncle()->_color = NOIR;
+			else if (n->get_oncle()->_color == ROUGE) {
+				n->get_parent()->_color = NOIR;
+				n->get_oncle()->_color = NOIR;
 
-				node *g = this->get_grandparent();
+				pointer g = n->get_grandparent();
 				g->_color = ROUGE;
 				g.insert_repare();
 			}
 			else {
-				node *p = this->get_parent();
-				node *g = this->get_grandparent();
+				pointer p = n->get_parent();
+				pointer g = n->get_grandparent();
 
 				if (this == g->_left->_right) {
-					this->left_rotation();
-					this = this->_left;
+					n->left_rotation();
+					this = n->_left;
 				}
 				else if (this == g->_right->_left) {
-					this->right_rotation();
-					this = this->_right;
+					n->right_rotation();
+					this = n->_right;
 				}
 
-				node *p2 = this->get_parent();
-				node *g2 = this->get_grandparent();
+				pointer p2 = n->get_parent();
+				pointer g2 = n->get_grandparent();
 				
 				if (this == p2->_left)
-					this->right_rotation();
+					n->right_rotation();
 				else
-					this->left_rotation();
+					n->left_rotation();
 				
 				p2->_color = NOIR;
 				g2->_color = ROUGE;
@@ -253,7 +344,7 @@ namespace ft
 			}
 		}
 
-		node *insert_node(node *new_node) {
+		void insert_node(pointer new_node) {
 			/*
 				Regles a respecté pour un arbre binaire rouge et noir.
 
@@ -286,27 +377,82 @@ namespace ft
 			new_node->parent = this;
 			new_node->_right = NULL;
 			new_node->_left = NULL;
-			new_node = ROUGE;
+			new_node->_color = ROUGE;
 
 			// Equilibrer arbre si il le faut
 			new_node->insert_repare();
 			
 			while (this->parent() != NULL)
 				this = this->parent();
-			// Return the racine (the potential new one or the old one)
-			return (this);
+			// Return la racine
 		};
 
+		pointer search_node_to_swap() {
+			pointer tmp = this;
+			Key key_to_swap = this->_key;
+
+			// Faire un check sur la longueur du sous arbre gauche et droit pour savoir dans quel sous arbre delete le noeud
+
+			this = this->_right;
+			while (this->_left->_left != NULL)
+				this = this->_left;
+			tmp->_key = this->_key;
+			this->_key = key_to_swap;
+			return (this);
+		}
+
+		void delete_node(pointer to_delete) {
+			/*
+				Pour commencé on recherche le noeud a supprimé dans l'arbre.
+				Pour pouvoir supprimé le noeud correctement il faut que celui-ci ait au plus un enfant qui ne soit pas une feuille (noeud NULL).
+				Si le noeud contient deux enfants qui ne sont pas des feuilles il faut recherché soit le successeur le plus proche (le noeud le plus a gauche du sous arbre droit)
+				ou le predecesseur le plus proche (le noeud le plus a droite du sous arbre gauche)
+
+			*/
+
+			pointer racine = this;
+			while (this->_key != to_delete->_key) {
+				if (to_delete->_key > this->_key) {
+					if (this->_right == NULL)
+						return (NULL);
+					this = this->_right;
+				}
+				else {
+					if (this->_left == NULL)
+						return (NULL);
+					this = this->_left;
+				}
+			}
+			if (this->_left != NULL && this->_right != NULL)
+				this = search_node_to_swap();
+
+			if (this->_left == NULL) {
+				if (this == this->_parent->_right) {
+					this->_parent->_right = this->_right;
+					this->_right->_parent = this->_parent;
+				} else {
+					this->_parent->_left = this->_right;
+					this->_right->_parent = this->_parent;
+				}
+				_alloc.destroy(this);
+			}
+			else {
+				if (this == this->_parent->_right) {
+					this->_parent->_right = this->_left;
+					this->_right->_parent = this->_parent;
+				} else {
+					this->_parent->_left = this->_left;
+					this->_left->_parent = this->_parent;
+				}
+				_alloc.destroy(this);
+			}
+		}
 
 		private :
-
-			node(const node& src);
-
-			node	*_left;
-			node	*_right;
-			node	*_parent;
-			int		_color;
-			Key		_key;
+			pointer _root;
+			value_compare _comp;
+			allocator_type	_alloc;
+			
 	};
 
 	template < class Key,                                     // map::key_type
@@ -326,8 +472,6 @@ namespace ft
 
 			typedef Compare key_compare;
 
-			typedef Compare value_compare;
-
 			typedef Alloc allocator_type;
 
 			typedef typename allocator_type::reference reference;
@@ -338,17 +482,30 @@ namespace ft
 			
 			typedef typename allocator_type::const_pointer const_pointer;
 
-			typedef typename ft::mapIterator<value_type> iterator;
+			typedef typename ft::mapIterator<key_type, Compare> iterator;
 
-			typedef typename ft::mapIterator<value_type> const_iterator;
+			// typedef type name ft::mapIterator<value_type> const_iterator;
 
-			typedef typename ft::mapIterator<iterator> reverse_iterator;
+			// typedef typename ft::mapIterator<iterator> reverse_iterator;
 
-			typedef typename ft::mapIterator<const_iterator> const_reverse_iterator;
+			// typedef typename ft::mapIterator<const_iterator> const_reverse_iterator;
 
 			typedef typename allocator_type::size_type	size_type;
 
 			typedef typename std::ptrdiff_t difference_type;
+
+			class value_compare : public std::binary_function<value_type, value_type, bool>
+			{
+				protected:
+					key_compare comp;
+					value_compare(Compare c) : comp(c) {} 
+				public:
+					bool operator() (const value_type& x, const value_type& y) const {
+						return comp(x.first, y.first);
+					}
+			}
+
+			typedef typename ft::tree<value_type, value_compare> tree,
 
 
 			// ---------------------- Constructor / Destructor / Asigment operator ---------------------------
@@ -360,13 +517,13 @@ namespace ft
 			// Range Constructor
 			template <class InputIterator>
 			map(InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _comp(comp) {
-				difference_type size = last - first;
-				_alloc_size = size;
-				_pair = _alloc.allocate(size);
-				for (ft::map<Key, T>::iterator it(first); it != end; it++) {
-					_alloc.construct(_pair + _fill_size, ft::pair<(*it).first, (*it).second>);
-					_fill_size++;
-				}
+				// difference_type size = last - first;
+				// _alloc_size = size;
+				// _pair = _alloc.allocate(size);
+				// for (ft::map<Key, T>::iterator it(first); it != end; it++) {
+				// 	_alloc.construct(_pair + _fill_size, ft::pair<(*it).first, (*it).second>);
+				// 	_fill_size++;
+				// }
 			};
 
 			// Copy Constructor
@@ -392,8 +549,8 @@ namespace ft
 
 			// ---------------------- Capacity ---------------------------
 
-			bool empty() const { return (!this->_fill_size ? true : false); };
-			size_type size() const { return (this->_fill_size); };
+			bool empty() const;
+			size_type size() const;
 			size_type max_size() const;
 
 			// ---------------------- Element Access ---------------------------
@@ -447,9 +604,7 @@ namespace ft
 		private :
 			Alloc		_alloc;
 			Compare		_comp;
-			node		*_node;
-			size_type	_fill_size;
-			size_type	_alloc_size;
+			tree		_tree;
 	};
 }
 
